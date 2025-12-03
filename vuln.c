2806571c2f0d52c4757244742dfba52f4080a627
squests <sean.phi@sjsu.edu>
2025-12-03 00:03:52 +0000

    Update vuln.c

--- vuln.c
+++ vuln.c
@@ -8,14 +8,12 @@ void win(){
 int vuln() {
     char buffer[300];
     while(fgets(buffer, sizeof(buffer), stdin)) {
-
-        printf(buffer);
-    }
-return 0;
+		printf(buffer);
+	}
+	return 0;
 }
 
 int main() {
     vuln();
-
     return 0;
 }
